c4b55c21cfaa51d030294b057a963531de54c5d8
neop-haw <filling_box@yahoo.com>
2021-07-01 02:45:07 +0300

    Added switching of questions.

--- myQuiz/urls.py
+++ myQuiz/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'myQuiz'
 urlpatterns = [
     path('', views.index),
-    path('<int:question_id>', views.question, name='question'),
+    path('<int:question_id>/', views.question, name='question'),
+    path('<int:question_id>/error', views.vote, name='vote'),
     path('results', views.results, name='results'),
 ]
--- myQuiz/views.py
+++ myQuiz/views.py
@@ -16,11 +16,10 @@ def index(request):
     return render(request, 'myQuiz/index.html')
 
 
-def question(request, question_id):
-
+def vote(request, question_id):
     q = get_object_or_404(Question, pk=question_id)
     try:
-        current_a = q.answer_set.get(pk=request.POST['answer'])
+        current_a = q.answer_set.get(pk=request.POST['ans'])
     except (KeyError, Answer.DoesNotExist):
         # Redisplay the question voting form.
         return render(request, 'myQuiz/questionLayout.html', {
@@ -30,8 +29,13 @@ def question(request, question_id):
     else:
         current_a.is_checked = True
         current_a.save()
-    return HttpResponseRedirect(reverse('myQuiz:question', args=(question_id,)))
-    # return render(request, 'myQuiz/questionLayout.html', {'question': q, 'question_id': question_id})
+        return HttpResponseRedirect(reverse('myQuiz:question', args=(question_id+1,)))
+        # return render(request, 'myQuiz/questionLayout.html', {'question': q, 'question_id': question_id})
+
+
+def question(request, question_id):
+    q = get_object_or_404(Question, pk=question_id)
+    return render(request, 'myQuiz/questionLayout.html', {'question': q})
 
 
 def results(request):
