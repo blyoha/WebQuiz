86228e0e613a2f2f40adb068f67462ee6c92efe0
neop-haw <filling_box@yahoo.com>
2021-07-03 13:41:16 +0300

    Removed validation check.

--- myQuiz/views.py
+++ myQuiz/views.py
@@ -21,24 +21,17 @@ def index(request):
 def vote(request, question_id):
     q = get_object_or_404(Question, pk=question_id)
     checked_ans = list()
-    try:
-        for a in request.POST.getlist('ans'):
-            checked_ans.append(q.answer_set.get(pk=a))
-            # current_a = q.answer_set.get(pk=request.POST.getlist('ans'))
-    except (KeyError, Answer.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(request, 'myQuiz/questionLayout.html', {
-            'question': q,
-            'error_message': "Пожалуйста, выберите ответ.",
-        })
-    else:
-        for a in checked_ans:
-            a.is_checked = True
-            a.save()
+    for a in request.POST.getlist('ans'):
+        checked_ans.append(q.answer_set.get(pk=a))
+        # current_a = q.answer_set.get(pk=request.POST.getlist('ans'))
 
-        if q.id == 5:
-            return HttpResponseRedirect(reverse('myQuiz:results'))
-        return HttpResponseRedirect(reverse('myQuiz:question', args=(question_id+1, )))
+    for a in checked_ans:
+        a.is_checked = True
+        a.save()
+
+    if q.id == 5:
+        return HttpResponseRedirect(reverse('myQuiz:results'))
+    return HttpResponseRedirect(reverse('myQuiz:question', args=(question_id+1, )))
 
 
 def question(request, question_id):
